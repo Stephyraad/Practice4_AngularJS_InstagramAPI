75d56cfedfbcfcd8f8455a66cf4dd5c2356b629a
Stephanie Raad <stephy.raad@gmail.com>
2014-10-24 12:10:40 -0700

    added error messages

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 var myApp= angular.module("myApp", []);
 
-myApp.controller("MyController", function($scope, $http){
+myApp.controller("MyController", function($scope, $http, $timeout){
 
-	// $scope.errorMessage= false;
-	// $scope.images= null;
-	$scope.submit=function(keyword){
+	$scope.errorMessage= false;
+	$scope.searchMessage= false;
 
+	var validateForm= function(){
+		if($scope.myForm.$valid){
+			return true;
+		}else{
+			$scope.errorMessage="You must Submit a Tag field";
+			return false;
+			}
+	};	
 	
+	$scope.submit=function(keyword){
+		if(validateForm()){
+			$scope.searchMessage="Searching Instagram for "+ $scope.keyword;
+			$scope.errorMessage=false;
 
-		var url = "https://api.instagram.com/v1/tags/" + $scope.keyword + "/media/recent";
-            
-		var request = {
-                client_id : "0c7279e3eddd47959eea95d6a3a3c83f",
-                callback: 'JSON_CALLBACK'
-            };
-            
-        $http({
-                method: 'JSONP',
-                url: url,
-                params: request
-            }).
-                success(function(response) {
-                    
-                    $scope.results = response.data;
-                }).
-                error(function() {
-                    alert('error');
-                });
-		};
+			var url = "https://api.instagram.com/v1/tags/" + $scope.keyword + "/media/recent";
+	            
+			var request = {
+	                client_id : "0c7279e3eddd47959eea95d6a3a3c83f",
+	                callback: 'JSON_CALLBACK'
+	            };
+	            
+	        $http({
+	                method: 'JSONP',
+	                url: url,
+	                params: request
+	            }).
+	                success(function(response) {
+	                	//if the call is a success
+						
+	                    
+	                    $scope.results = response.data;
+	                    $scope.searchMessage=false;
+
+	         
+
+	                }).
+	                error(function() {
+	                    alert('error');
+	                });
+			};
+			$scope.keyword=null;
+		} //end of if
+
 });
 
 
