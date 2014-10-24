30a5697079ac0a63a55b1b1ed1768923a3d9e565
Stephanie Raad <stephy.raad@gmail.com>
2014-10-24 12:44:49 -0700

    success and timeout functions working

--- script.js
+++ script.js
@@ -18,35 +18,29 @@ myApp.controller("MyController", function($scope, $http, $timeout){
 		if(validateForm()){
 			$scope.searchMessage="Searching Instagram for "+ $scope.keyword;
 			$scope.errorMessage=false;
-
-			var url = "https://api.instagram.com/v1/tags/" + $scope.keyword + "/media/recent";
-	            
+			var url = "https://api.instagram.com/v1/tags/" + $scope.keyword + "/media/recent";     
 			var request = {
 	                client_id : "0c7279e3eddd47959eea95d6a3a3c83f",
 	                callback: 'JSON_CALLBACK'
 	            };
-	            
 	        $http({
-	                method: 'JSONP',
-	                url: url,
-	                params: request
-	            }).
-	                success(function(response) {
+	                method: 'JSONP',url: url,params: request
+	            }).success(function(response) {
 	                	//if the call is a success
-						
-	                    
-	                    $scope.results = response.data;
-	                    $scope.searchMessage=false;
-
-	         
+	                	if(response.meta.code===200){
+	                		if(response.data.length> 5){
+	                			$timeout(function() {$scope.results = response.data;}, 10);
+	                			$scope.searchMessage="You have" + response.data.length+ "pictures related " + $scope.keyword;
+	                		}else{$scope.searchMessage="Sorry we couldnt find any pictures related to "+ $scope.keyword;}
+	                	}
 
 	                }).
 	                error(function() {
 	                    alert('error');
 	                });
-			};
-			$scope.keyword=null;
-		} //end of if
+			} //end of if valid Form
+			
+		} //end of function(keyword) 
 
 });
 
