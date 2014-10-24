c43260869356e51635bf1b082b9883e32cff8501
Stephanie Raad <stephy.raad@gmail.com>
2014-10-24 11:19:05 -0700

    Ajax call working

--- script.js
+++ script.js
@@ -3,30 +3,36 @@ var myApp= angular.module("myApp", []);
 myApp.controller("MyController", function($scope, $http){
 
 	// $scope.errorMessage= false;
+	// $scope.images= null;
 	$scope.submit=function(keyword){
-		var url="https://api.instagram.com/v1/tags/"+keyword+"/media/recent";
-		var request={
-			client_id: "0c7279e3eddd47959eea95d6a3a3c83f",
-			outputMode: "json",
-			showSourceText: "1",
-			jsonp: "JSON_CALLBACK"
+
+	
+
+		var url = "https://api.instagram.com/v1/tags/" + $scope.keyword + "/media/recent";
+            
+		var request = {
+                client_id : "0c7279e3eddd47959eea95d6a3a3c83f",
+                callback: 'JSON_CALLBACK'
+            };
+            
+        $http({
+                method: 'JSONP',
+                url: url,
+                params: request
+            }).
+                success(function(response) {
+                    
+                    $scope.results = response.data;
+                }).
+                error(function() {
+                    alert('error');
+                });
 		};
-
-	$http({
-		method: "JSONP",
-		url: url,
-		params: request
-	}).
-	success(function(results){
-		$scope.results= results;
-
-	}).
-	error(function(){
-		alert("error");
-	})
-
-
-	};
-
-
 });
+
+
+
+
+
+
+
